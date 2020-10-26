53b3521cd21c422fda60f3390835626fdd4a55f1
Robert Roskam <raiderrobert@gmail.com>
2020-10-26 14:59:23 -0400

    my ractor with tests!

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,39 +1,47 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
+
+BRIE = "Aged Brie"
+BACKSTAGE_PASS = "Backstage passes to a TAFKAL80ETC concert"
+SULFURAS = "Sulfuras, Hand of Ragnaros"
+
+
+def step_quality(item, rate=-1):
+    item.quality = item.quality + rate if 0 <= item.quality + rate <= 50 else item.quality
+
+
+def update_quality(item):
+    if item.name not in {BRIE, BACKSTAGE_PASS, SULFURAS}:
+        # Handle decaying
+        if item.sell_in < 0:
+            step_quality(item, -2)
+        else:
+            step_quality(item, -1)
+    elif item.name == BACKSTAGE_PASS:
+        if item.sell_in <= 0:
+            # Quality drops to 0 after the concert
+            step_quality(item, item.quality*-1)
+        elif 0 < item.sell_in < 6:
+            step_quality(item, 3)
+        elif 6 <= item.sell_in < 11:
+            step_quality(item, 2)
+        elif 11 < item.sell_in:
+            step_quality(item, 1)
+    elif item.name == BRIE:
+        step_quality(item, 1)
+
+    if item.name != SULFURAS:
+        item.sell_in = item.sell_in - 1
+
+
+class GildedRose:
 
     def __init__(self, items):
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            update_quality(item)
 
 
 class Item:
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,14 +1,79 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from gilded_rose import Item, GildedRose
+from gilded_rose import Item, GildedRose, update_quality
+
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual("foo", items[0].name)
+
+    def test_brie(self):
+        item = Item(name="Aged Brie", sell_in=2, quality=0)
+
+        update_quality(item)
+        self.assertEqual(1, item.sell_in)
+        self.assertEqual(1, item.quality)
+
+        update_quality(item)
+        self.assertEqual(0, item.sell_in)
+        self.assertEqual(2, item.quality)
+
+        update_quality(item)
+        self.assertEqual(-1, item.sell_in)
+        self.assertEqual(3, item.quality)
+
+    def test_sulfuras(self):
+        item = Item(
+            name="Sulfuras, Hand of Ragnaros",
+            sell_in=0,
+            quality=80
+        )
+
+        update_quality(item)
+        self.assertEqual(0, item.sell_in)
+        self.assertEqual(80, item.quality)
+
+    def test_backstage(self):
+        item = Item(
+            name="Backstage passes to a TAFKAL80ETC concert",
+            sell_in=11,
+            quality=20
+        )
+
+        update_quality(item)
+        self.assertEqual(10, item.sell_in)
+        self.assertEqual(20, item.quality)
+
+        update_quality(item)
+        self.assertEqual(9, item.sell_in)
+        self.assertEqual(22, item.quality)
+
+        for _ in range(4):
+            update_quality(item)
+        self.assertEqual(5, item.sell_in)
+        self.assertEqual(30, item.quality)
+
+        update_quality(item)
+        self.assertEqual(4, item.sell_in)
+        self.assertEqual(33, item.quality)
+
+        for _ in range(5):
+            update_quality(item)
+        self.assertEqual(-1, item.sell_in)
+        self.assertEqual(0, item.quality)
+
+        update_quality(item)
+        self.assertEqual(-2, item.sell_in)
+        self.assertEqual(0, item.quality)
+
+        update_quality(item)
+        self.assertEqual(-3, item.sell_in)
+        self.assertEqual(0, item.quality)
+
 
 if __name__ == '__main__':
     unittest.main()
